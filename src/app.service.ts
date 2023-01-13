ae7639a9a6b8c88bdf61d6009b955c889e560b29
Jang-Jaewon <jewon119@gmail.com>
2023-01-13 14:52:18 +0900

    Add: process.env보단 ConfigService를 상용하는 것을 추천

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
 
   getTestEnv(): string {
-    return process.env.SECRET;
+    return this.configService.get('NAME');
   }
 }
