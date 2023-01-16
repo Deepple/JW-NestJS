12984eb6fc3bfeddd96e149cbcf04d805692a4f4
Jang-Jaewon <jewon119@gmail.com>
2023-01-16 09:24:08 +0900

    Add: Api Router 뼈대 만들어놓기

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('api/users')
+export class UsersController {
+  constructor(private UsersService: UsersService) {}
+
+  @Get()
+  getUsers(@Req() req) {
+    return req.user;
+  }
+
+  @Post()
+  postUsers(@Body() body: JoinRequestDto) {
+    this.UsersService.postUsers(body.email, body.nickname, body.password);
+  }
+
+  @Post('login')
+  logIn(@Req() req) {
+    return req.user;
+  }
+
+  @Post('logout')
+  logOut(@Req() req, @Res() res) {
+    req.logOut();
+    res.clearCookie('connect.sid', { httpOnly: true });
+    res.send('ok');
+  }
+}
