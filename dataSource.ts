aa62d71de8f3f089279756e4082ebf13f0a8f5d6
Jang-Jaewon <jewon119@gmail.com>
2023-01-27 10:44:17 +0900

    Add: migration rollback test(npm run db:migrate:revert)

--- dataSource.ts
+++ dataSource.ts
@@ -13,8 +13,8 @@ dotenv.config();
 
 const dataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
@@ -30,8 +30,8 @@ const dataSource = new DataSource({
   ],
   migrations: [__dirname + '/src/migrations/*.ts'],
   synchronize: false,
-  logging: true,
   charset: 'utf8mb4',
+  logging: true,
 });
 
 export default dataSource;
