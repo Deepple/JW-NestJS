69f2e890e997fe5c44b593d2a6b839cdcd5b91bf
Jang-Jaewon <jewon119@gmail.com>
2023-01-30 09:23:31 +0900

    Add: Loging & Logout Guard 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,16 +4,19 @@ import {
   Get,
   Post,
   Req,
-  Res, UseGuards,
+  Res,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {ApiCookieAuth, ApiOperation, ApiResponse, ApiTags} from '@nestjs/swagger';
 import { UserDto } from '../common/dto/user.dto';
 import { User } from '../common/decorators/user.decorator';
 import { UndefinedToNullInterceptor } from '../common/interceptors/undefinedToNull.interceptor';
-import {LocalAuthGuard} from "../auth/local-auth.guard";
+import { LocalAuthGuard } from '../auth/local-auth.guard';
+import { NotLoggedInGuard } from '../auth/not-logged-in.guard';
+import { LoggedInGuard } from '../auth/logged-in.guard';
 
 @UseInterceptors(UndefinedToNullInterceptor)
 @ApiTags('USER')
@@ -27,10 +30,11 @@ export class UsersController {
   @ApiOperation({ summary: '내 정보 조회' })
   @Get()
   getUsers(@User() user) {
-    return user;
+    return user || false;
   }
 
   @ApiOperation({ summary: '회원가입' })
+  @UseGuards(NotLoggedInGuard)
   @Post()
   async JoinUsers(@Body() userData: JoinRequestDto) {
     return await this.usersService.JoinUsers(userData);
@@ -48,11 +52,12 @@ export class UsersController {
     return user;
   }
 
+  @ApiCookieAuth('connect.sid')
   @ApiOperation({ summary: '로그아웃' })
+  @UseGuards(LoggedInGuard)
   @Post('logout')
-  logOut(@Req() req, @Res() res) {
-    req.logOut();
+  async logOut(@Res() res) {
     res.clearCookie('connect.sid', { httpOnly: true });
-    res.send('ok');
+    return res.send('ok');
   }
 }
